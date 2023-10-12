d2e538753f170e257de8cb0a09e9978d15631eeb
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2023-10-12 18:45:09 +0300

    login systeemi toimii

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require('express');
 //const port=3000;
 const studentRouter=require('./controllers/student');
 const userRouter=require('./controllers/user');
+const loginRouter=require('./controllers/login');
 
 const app=express();
 const dotenv=require('dotenv');
@@ -13,6 +14,7 @@ app.use(express.urlencoded({extended:false}));
 
 app.use('/student',studentRouter);
 app.use('/user',userRouter);
+app.use('/login',loginRouter);
 
 
 app.listen(port,function(){
